9be49d005910d7c5769b0274c08508d94dd05383
nantawat23308 <nantawat23308@gmail.com>
2022-05-08 00:12:39 +0700

    initial commit

---
+++ Except/number.py
@@ -0,0 +1,19 @@
+#except value error 
+#use try and except to except value error
+
+#NameError error ใน state ment error เลยทำให้ ไม่มี formate ของ x เลย print x ไม้่ได้ แก้โดยใช้ else
+
+#ใช้ loop และ break ใน การรับ input เรื่อยกว่าจะไม่ value error
+while True:
+
+    try:
+        x = int(input("what is x: "))
+    
+    except ValueError:
+        print("x is not integer")
+
+    else:
+        break
+        
+
+print(f" x is {x}")
